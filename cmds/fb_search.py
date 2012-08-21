b3667c1156708ed64da7722d0123e8b08cdb1052
Samuel A. Winchenbach <swinchen@gmail.com>
2012-08-21 16:57:56 -0400

    Added a flag so you can also search based on description

--- cmds/fb_search.py
+++ cmds/fb_search.py
@@ -7,13 +7,19 @@ from lib import config
 
 def cmd(args):
     # print('search: %r' % args)   
-    dl = config.FBDownloadList()   
-    plugs = [p for p in dl.plugins() if re.search(args.pattern, p)]
-    descs = map(lambda p: dl.parser[p].get('description'), plugs)
-    for p, d in zip(plugs, descs):
+    dl = config.FBDownloadList()
+    data = list(zip(dl.plugins(), list(dl.parser[p].get('description') for p in dl.plugins())))
+
+    if args.d:
+        plugs = [(k,v) for k,v in data if re.search(args.pattern, ' '.join([k, v]))]
+    else:
+        plugs = [(k,v) for k,v in data if re.search(args.pattern, k)]
+    
+    for p, d in plugs:
         print('%s\n\t%s\n' % (p, d))
 
 def add_parser(sub):
     p = sub.add_parser('search', help=__doc__, description=__doc__)
-    p.add_argument('pattern', help='a pattern to search plugin names for', nargs='?' ,default='.')
+    p.add_argument('pattern', help='a pattern to search plugin names for', nargs='?', default='.')
+    p.add_argument('-d', help='search plugin description, as well as plugin name', action='store_true')
     p.set_defaults(func=cmd)
