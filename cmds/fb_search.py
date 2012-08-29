8200ddd303ecebf47070cb8844a5ed252878d215
Samuel A. Winchenbach <swinchen@gmail.com>
2012-08-28 22:51:13 -0400

    search displays various versions available for download

--- cmds/fb_search.py
+++ cmds/fb_search.py
@@ -13,9 +13,10 @@ def cmd(args):
         plugs = [(p,d) for p,d in data if re.search(args.pattern, ' '.join([p, d]), re.IGNORECASE)]
     else:
         plugs = [(p,d) for p,d in data if re.search(args.pattern, p, re.IGNORECASE)]
-    
+
     for p, d in plugs:
-        print('%s\n\t%s\n' % (p, d))
+        options = ', '.join([o for o in dl.options(p) if o != 'description' and o != 'format'])
+        print('%s [%s]\n\t%s\n' % (p, options, d))
 
 def add_parser(sub):
     p = sub.add_parser('search', help=__doc__, description=__doc__)
